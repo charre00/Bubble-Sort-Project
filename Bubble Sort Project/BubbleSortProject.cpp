32e6fb96f3e0db8e86a8ed5a3ae4b37b0b1bb19a
Christopher Harrelson <charre2016@gmail.com>
2018-02-19 09:02:28 -0500

    First Commit

--- Bubble Sort Project/BubbleSortProject.cpp
+++ Bubble Sort Project/BubbleSortProject.cpp
@@ -1,11 +1,38 @@
-// BubbleSortProject.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-
+#include <utility>
+#include <iostream>
 
 int main()
 {
-    return 0;
+	int array[] = { 6, 3, 2, 9, 7, 1, 5, 4, 8 };
+	const int length = sizeof(array) / sizeof(array[0]);
+	int outerBoundIndex = length;
+	for (int count = 1; count < length; ++count) 
+	{
+		bool Swaps = false;
+		for (int startingIndex = 0; startingIndex < outerBoundIndex - 1; ++startingIndex)
+		{
+			int currentIndex = startingIndex + 1;
+			if (array[currentIndex] < array[startingIndex])
+			{
+				std::swap(array[currentIndex], array[startingIndex]);
+				Swaps = true;
+			}
+		}
+		if (!Swaps)
+		{
+			std::cout << "Early termination on iteration " << count << ". \n";
+			break;
+		}
+		--outerBoundIndex;
+	}
+	for (int index = 0; index < length; ++index)
+	{
+		std::cout << array[index] << ' ';
+	}
+	std::cin.get();
+	std::cin.ignore(32767, '\n');
+	std::cin.clear();
+	return 0;
 }
 
